94a50f7d9b3ddbfa58dd06382c2a1fdb851ec0d6
Emberist <emberistemac@proton.me>
2024-11-12 17:30:47 +0100

    fix: options error

--- src/core/utils/updateTaskIfDatabaseServiceAvailable.ts
+++ src/core/utils/updateTaskIfDatabaseServiceAvailable.ts
@@ -1,5 +1,5 @@
 import type { PublicKey } from "@solana/web3.js";
-import { Console, Effect, identity } from "effect";
+import { Console, Effect, Option } from "effect";
 import { constNull } from "effect/Function";
 import {
 	DatabaseService,
@@ -8,8 +8,10 @@ import {
 
 export const updateTaskIfDatabaseServiceAvailable = (param: UpdateTaskParams) =>
 	Effect.serviceOption(DatabaseService).pipe(
-		Effect.flatMap(identity),
-		Effect.flatMap((service) => service.updateTaskStatus(param)),
+		Effect.map(Option.getOrNull),
+		Effect.flatMap((service) =>
+			service ? service.updateTaskStatus(param) : Effect.succeed(null),
+		),
 		Effect.tapErrorTag("UpdateTaskStatusError", (error) =>
 			Console.log("Error updating task", error),
 		),
@@ -18,12 +20,11 @@ export const updateTaskIfDatabaseServiceAvailable = (param: UpdateTaskParams) =>
 
 export const updateCreditsIfDatabaseServiceAvailable = (pbk: PublicKey) =>
 	Effect.serviceOption(DatabaseService).pipe(
-		Effect.flatMap(identity),
-		Effect.flatMap((service) => service.removeUserCredit(pbk)),
-		Effect.tapErrorTag("UpdateCreditsFieldError", (error) =>
-			Console.log("Error updating user", error),
+		Effect.map(Option.getOrNull),
+		Effect.flatMap((service) =>
+			service ? service.removeUserCredit(pbk) : Effect.succeed(null),
 		),
-		Effect.tapErrorTag("NoSuchElementException", (error) =>
+		Effect.tapErrorTag("UpdateCreditsFieldError", (error) =>
 			Console.log("Error updating user", error),
 		),
 		Effect.orElseSucceed(constNull),
