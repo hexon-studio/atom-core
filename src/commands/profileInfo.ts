561cc604d06d6f209b86448da45fa72753d19db2
Emberist <13602797+emberist@users.noreply.github.com>
2025-03-24 22:56:33 +0100

    chore: update profile info command

--- src/commands/profileInfo.ts
+++ src/commands/profileInfo.ts
@@ -1,21 +1,13 @@
-import { Effect, LogLevel, Logger } from "effect";
-import { GameService } from "../core/services/GameService";
+import { Console, Effect } from "effect";
+import { jsonStringify } from "~/utils/jsonStringify";
 import { getGameContext } from "../core/services/GameService/utils";
 import type { GlobalOptions } from "../types";
 import { makeAtomCommand } from "./makeAtomCommand";
 
 export const runProfileInfo = async (globalOpts: GlobalOptions) =>
 	makeAtomCommand(() =>
-		GameService.pipe(
-			Effect.tap((service) =>
-				service.initGame(service.gameContext, globalOpts),
-			),
-			Effect.tap(() => Effect.log("Game initialized.")),
-			Effect.flatMap(getGameContext),
-			Effect.map((context) => context.playerProfile),
-			Effect.tap((profile) =>
-				Effect.log("Profile fetched.").pipe(Effect.annotateLogs({ profile })),
-			),
-			Logger.withMinimumLogLevel(LogLevel.Debug),
+		getGameContext().pipe(
+			Effect.map((context) => jsonStringify(context.playerProfile)),
+			Effect.tap(Console.log),
 		),
 	)(globalOpts);
